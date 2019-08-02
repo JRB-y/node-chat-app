3bb46aa69086a55c0a67f2052b1585d53c3f4753
jrb <jrb.youssef@gmail.com>
2019-08-03 00:39:36 +0100

    move the js to public/js

---
+++ public/js/client.js
@@ -0,0 +1,49 @@
+$(function () {
+    let socket = io.connect();
+    // chat vars
+    let $messageArea = $('#messageArea');
+    let $messageForm = $('#messageForm');
+    let $chat = $('#chat')
+    let $message = $('#message');
+    // user vars
+    let $userFormArea = $('#userFormArea');
+    let $userForm = $('#userForm');
+    let $username = $('#username');
+    // users area
+    let $users = $('#users');
+
+    // submit message
+    $messageForm.submit(e => {
+        e.preventDefault();
+        socket.emit('send-message', $message.val());
+        $message.val('');
+        // console.log($message.val());
+    });
+
+    // user login
+    $userForm.submit(e => {
+        e.preventDefault();
+        socket.emit('user-login', $username.val(), (data) => {
+            if (data) {
+                $userFormArea.hide();
+                $messageArea.css('display', 'flex');
+            }
+        });
+        $username.val('');
+        // console.log($message.val());
+    });
+
+    socket.on('new-message', data => {
+        $chat.append('<div class="card"><strong>' + data.user + '</strong>' + data.msg +
+            '</div>');
+    })
+
+    socket.on('get-users', (data) => {
+        let html = "";
+        for (i = 0; i < data.length; i++) {
+            html += '<li class="list-group-item"> ' + data[i] + ' </li>';
+        }
+        $users.html(html);
+    })
+
+});
